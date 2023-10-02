501ff12182ffaa8d22df6615ddddcb1b8234ae92
Matthew McKenzie <matthew.mckenzie@aa.com>
2023-10-02 10:58:53 -0700

    added prismic access token

--- prismicio.ts
+++ prismicio.ts
@@ -30,9 +30,7 @@ const routes: prismic.ClientConfig["routes"] = [
  *
  * @param config - Configuration for the Prismic client.
  */
-export const createClient = (config: prismicNext.CreateClientConfig = {
-
-}) => {
+export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     routes,
