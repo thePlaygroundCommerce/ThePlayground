9cf47e6e993f69e647e4792ecc2f3aac5681eca9
Matthew McKenzie <matthew.mckenzie@aa.com>
2023-12-28 17:11:16 -0700

    changed env var

--- constants/index.ts
+++ constants/index.ts
@@ -6,20 +6,7 @@ export const DEFAULT_FETCH_INIT = {
   },
 };
 
-export const SQUARE_URL = process.env.SQUARE_ENV
-export const CONFIG = {
-  square: {
-    development: {
-      // url: "http://localhost:3000/",
-      url: "https://square-backend-stage-ac8a01ff6dd6.herokuapp.com/",
-    },
-    production: {
-      url: "https://square-backend.herokuapp.com/",
-    },
-    test: {
-      url: "",
-    },
-  },
-};
+export const SQUARE_URL = process.env.NEXT_PUBLIC_SQUARE_ENV
+
 
 // export { default as SOCIAL_MEDIA_COMPONENT_MAP } from "./SocialMediaComponentMap";
