30d05f690b8581f43ef9ef1b300faeb1c13b9780
Matthew McKenzie <matthew.mckenzie@aa.com>
2024-08-01 22:33:01 -0700

    satisfy typescript

--- app/shop/product/[slug]/page.tsx
+++ app/shop/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getProductDetails } from "api/catalogApi";
 import ProductDetailsModify from "components/ProductDetailsModify";
 import ProductImageGallery from "components/ProductImageGallery";
+import { CatalogObject } from "square";
 
 type Props = {
   params: { slug: string };
@@ -25,7 +26,7 @@ const Page = async (props: Props) => {
             <ProductImageGallery images={filteredRelatedImages} />
             <ProductDetailsModify
               catalogItemObject={catalogObject}
-              catalogImageObjects={filteredRelatedImages}
+              catalogImageObjects={filteredRelatedImages as CatalogObject[]}
             />
           </div>
         </div>
