b3e34b99f41e781413f73e7e6fcd47fda40312fe
Matthew McKenzie <matthew.mckenzie@aa.com>
2024-12-01 20:53:31 -0500

    modified product griid columns

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const ProductGrid = ({ catalogItems, catalogImages }: Props) => {
   return (
-    <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-3 md:grid-rows-3 gap-y-12 w-full max-w-full">
+    <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-3 lg:grid-cols-4 md:grid-rows-3 gap-y-12 w-full max-w-full">
       {!catalogItems ? (
         <EmptyProductGrid />
       ) : (
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440px",
+      xl: "1240px",
+      "2xl": "1440px",
     },
     // colors: {
     //   'blue': '#1fb6ff',
