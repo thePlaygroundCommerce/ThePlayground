bf65f10dadc81eb8335b05b40e063dc6e06af5db
Matthew McKenzie <matthew.mckenzie@aa.com>
2026-05-14 23:19:13 -0700

    remove debug

--- src/api/clients/square.ts
+++ src/api/clients/square.ts
@@ -3,9 +3,6 @@ import {
   SquareEnvironment as Environment,
 } from "square";
 
-console.log(process.env.SQUARE_ACCESS_TOKEN)
-console.log(process.env.VERCEL_ENV)
-
 export default new Client({
   token: process.env.SQUARE_ACCESS_TOKEN,
   logging: {
