237fc13190a7641f0d7e33fd71c1bbfc64153890
Matthew McKenzie <matthew.mckenzie@aa.com>
2026-07-15 21:26:26 -0700

    fix product gallery

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -380,7 +380,7 @@ export const WebflowSlider = ({
   useDrag(handleDrag, { target: ref, axis: "x" })
 
   return (
-    <div className="relative h-full">
+    <div className="relative h-full sm:h-auto">
       {withControls && (
         <div className="absolute -top-5 right-5 z-10 flex gap-2">
           {isPrevButtonVisible && (
